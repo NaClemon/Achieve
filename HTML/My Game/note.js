9fb32885950f8a83c3dffc2f70d421b912a6e95a
bloquer <995skh@gmail.com>
2018-05-13 20:36:32 +0900

    initial commit

--- HTML/My Game/note.js
+++ HTML/My Game/note.js
@@ -6,35 +6,41 @@ var line1 = $("#line1");
 var line2 = $("#line2");
 var line3 = $("#line3");
 var line4 = $("#line4");
+var combotext = document.getElementById("nowcombo");
+var judgetext = document.getElementById("nowjudge");
 var current_note = 0;
-var noteV = 2;
-var perfect = 0;
-var good = 0;
-var bad = 0;
+var perfect = 100;
+var good = 70;
+var bad = 30;
 var miss = 0;
 var combo = 0;
-var noteanim;
+var maxcombo = 0;
+var sum = 0;
+var perfectsum = 0;
+var goodsum = 0;
+var badsum = 0;
+var misssum = 0;
+
 var game;
-
-var n=$("#nowjudge");
-
-function note_down(note){
-    current_note = parseInt(note.css("top"));
-    if(current_note > 500)
-        current_note = 0;
-    note.css("top", current_note + noteV);
-}
-
-function game_start(){
-    note_down(note1);
-    game = requestAnimationFrame(game_start);
-}
+var opa;
 
 $(document).on('keydown', function(e){
     var key = e.keyCode;
     if(key == 65){
         line1.css("opacity", 0.5);
-        n.css("color", "red");
+        now_judge(keyjudge(1), 65);
+    }
+    else if(key == 83){
+        line2.css("opacity", 0.5);
+        now_judge(keyjudge(2), 83);
+    }
+    else if(key == 68){
+        line3.css("opacity", 0.5);
+        now_judge(keyjudge(3), 68);
+    }
+    else if(key == 70){
+        line4.css("opacity", 0.5);
+        now_judge(keyjudge(4), 70);
     }
 });
 
@@ -42,6 +48,136 @@ $(document).on('keyup', function(e){
     var key = e.keyCode;
     if(key == 65){
         line1.css("opacity", 0.3);
-        n.css("color", "black");
     }
-})
+    else if(key == 83){
+        line2.css("opacity", 0.3);
+    }
+    else if(key == 68){
+        line3.css("opacity", 0.3);
+    }
+    else if(key == 70){
+        line4.css("opacity", 0.3);
+    }
+});
+function disappear(){
+    judgetext.innerHTML = "";
+}
+
+function keyjudge(num){
+    if((num == 1) && (everything[subi] == 0)){
+        return 65;
+    }
+    else if((num == 2) && (everything[subi] == 1)){
+        return 83;
+    }
+    else if((num == 3) && (everything[subi] == 2)){
+        return 68;
+    }
+    else if((num == 4) && (everything[subi] == 3)){
+        return 70;
+    }
+    
+}
+
+function now_judge(keystate, key){
+    if(keystate == key){
+        if((noteY<=400)&&(noteY>=300)){
+            judgetext.innerHTML = "MISS";
+            setTimeout(disappear, 300);
+            combotext.innerHTML = "";
+            combo = 0;
+            noteY = 0;
+            bonus(miss);
+            misssum += 1;
+            subi++;
+        }
+        else if((noteY>400)&&(noteY<=450)){
+            judgetext.innerHTML = "BAD";
+            setTimeout(disappear, 300);
+            combotext.innerHTML = "";
+            combo = 0;
+            noteY = 0;
+            bonus(bad);
+            badsum += 1;
+            subi++;
+        }
+        else if((noteY>450)&&(noteY<=470)){
+            judgetext.innerHTML = "GOOD";
+            setTimeout(disappear, 300);
+            combo += 1;
+            noteY = 0;
+            bonus(good);
+            goodsum += 1;
+            subi++;
+        }
+        else if((noteY>470)&&(noteY<=488)){
+            judgetext.innerHTML = "PERFECT";
+            setTimeout(disappear, 300);
+            combo += 1;
+            noteY = 0;
+            bonus(perfect);
+            perfectsum += 1;
+            subi++;
+        }
+        else if((noteY>488)&&(noteY<=495)){
+            judgetext.innerHTML = "GOOD";
+            setTimeout(disappear, 300);
+            combo += 1;
+            noteY = 0;
+            bonus(good);
+            goodsum += 1;
+            subi++;
+        }
+        else if((noteY>495)&&(noteY<504)){
+            judgetext.innerHTML = "BAD";
+            setTimeout(disappear, 300);
+            combotext.innerHTML = "";
+            combo = 0;
+            noteY = 0;
+            bonus(bad);
+            badsum += 1;
+            subi++;
+        }
+    }
+    if(combo >= 3){
+        combotext.innerHTML = combo + "combo";
+        if(combo>=maxcombo)
+            maxcombo = combo;
+        opa = 1.0;
+        var id = setInterval(comopa, 50);
+        function comopa(){
+            if(opa < 0.8)
+                clearInterval(id);
+            opa -= 0.004;
+            combotext.style.opacity = opa;
+        }
+    }
+}
+
+function bonus(judge){
+    if(combo>=30){
+        sum += (judge*1.2);
+    }
+    else if(combo>=65){
+        sum += (judge*1.2*1.2);
+    }
+    else if(combo>=100){
+        sum += (judge*1.2*1.2*1.2);
+    }
+    else if(combo<30){
+        sum += judge;
+    }
+}
+
+function rankdecision(score){
+    if(score >= 13000)
+        return "S";
+    else if((score<13000)&&(score>=9000))
+        return "A";
+    else if((score<9000)&&(score>=6000))
+        return "B";
+    else if((score<6000)&&(score>=3000))
+        return "C";
+    else
+        return "F";
+}
--- HTML/My Game/script.js
+++ HTML/My Game/script.js
@@ -3,15 +3,28 @@ var cnt = canvas.getContext("2d");
 var mainscreen = document.getElementById("main");
 var choosescreen = document.getElementById("musicchoice");
 var gamescreen = document.getElementById("game");
+var resultscreen = document.getElementById("result");
 var anim = document.getElementById("anim");
+resultscreen.style.visibility = "hidden";
 choosescreen.style.visibility = "hidden";
 gamescreen.style.visibility = "hidden";
+var allresult = resultscreen.children;
 
 var mus1 = document.getElementById("mus1");
 var mus2 = document.getElementById("mus2");
 var song1 = new Audio("Shots.mp3");
 var song2 = new Audio("Trouble.mp3");
 var count = 1;
+var prelisten;
+var gameid;
+var gamenote;
+var i = 0;
+var subi = 0;
+var everything = [];
+
+var noteV = 3;
+var noteY = 0;
+var noteloc = Math.floor(Math.random() * 4);
 
 function start(){
     if(choosescreen.style.visibility === "hidden"){
@@ -24,16 +37,20 @@ function start(){
     }
     mus2.disabled = true;
     song1.play();
-    if(song1.currentTime > 30){
-        song1.pause();
-        song1.currentTime = 0;
+    prelisten = setInterval(songloop, 5);
+    function songloop(){
+        var time = song1.currentTime;
+        if(time > 40){
+            song1.currentTime = 0;
+        }
     }
 }
+
 function nextsong(){
-    song1.currentTime = 0;
-    song2.currentTime = 0;
     song1.pause();
     song2.play();
+    song1.currentTime = 0;
+    song2.currentTime = 0;
     var pos = 0;
     var scalepos = 0;
     if(count == 1){
@@ -50,9 +67,13 @@ function nextsong(){
         }
         mus2.disabled = false;
         mus1.disabled = true;
-        if(song2.currentTime > 30){
-            song2.pause();
-            song2.currentTime = 0;
+        clearInterval(prelisten);
+        prelisten = setInterval(songloop, 5);
+        function songloop(){
+            var time = song2.currentTime;
+            if(time > 40){
+                song2.currentTime = 0;
+            }
         }
         count = 0;
     }
@@ -60,10 +81,10 @@ function nextsong(){
         return false;
 }
 function previoussong(){
-    song1.currentTime = 0;
-    song2.currentTime = 0;
     song2.pause();
     song1.play();
+    song1.currentTime = 0;
+    song2.currentTime = 0;
     var pos = 0;
     var scalepos = 0;
     if(count == 0){
@@ -80,9 +101,13 @@ function previoussong(){
         }
         mus2.disabled = true;
         mus1.disabled = false;
-        if(song1.currentTime > 30){
-            song1.pause();
-            song1.currentTime = 0;
+        clearInterval(prelisten);
+        prelisten = setInterval(songloop, 5);
+        function songloop(){
+            var time = song1.currentTime;
+            if(time > 40){
+                song1.currentTime = 0;
+            }
         }
         count = 1;
     }
@@ -91,10 +116,13 @@ function previoussong(){
 }
 
 function startgame(num){
+    clearInterval(prelisten);
+    var volumes;
+    var gamesong;
     song1.pause();
     song2.pause();
-    song1.currentTime = 0;
-    song2.currentTime = 0;
+    song1.currentTime = 78;
+    song2.currentTime = 63.2;
     if(gamescreen.style.visibility == "hidden"){
         choosescreen.style.visibility = "hidden";
         gamescreen.style.visibility = "visible";
@@ -103,36 +131,66 @@ function startgame(num){
         gamescreen.style.visibility = "hidden";
     }
     if(num == 1){
+        song1.volume = 0.1;
         song1.play();
+        volumes = setInterval(bigvol, 30);
+        function bigvol(){
+            song1.volume += 0.01;
+            if(song1.volume >= 0.9)
+                clearInterval(volumes);
+        }
+        gamesong = setInterval(gameduration, 10);
+        gamenote = setInterval(randomnum, 100);
+        function gameduration(){
+            var time = song1.currentTime;
+            if(time > 218){
+                song1.volume -= 0.001;
+                if(time > 223){
+                    song1.pause();
+                    song1.currentTime = 0;
+                    song1.volume = 1;
+                    gameresult();
+                    initresult();
+                    clearInterval(gamesong);
+                    clearInterval(gamenote);
+                }
+            }
+        }
     }
     else if(num == 2){
+        song2.volume = 0.1;
         song2.play();
+        volumes = setInterval(bigvol, 30);
+        function bigvol(){
+            song2.volume += 0.01;
+            if(song2.volume >= 0.9)
+                clearInterval(volumes);
+        }
+        gamesong = setInterval(gameduration, 10);
+        gamenote = setInterval(randomnum, 100);
+        function gameduration(){
+            var time = song2.currentTime;
+            if(time > 180){
+                song2.volume -= 0.001;
+                if(time > 187){
+                    song2.pause();
+                    song2.currentTime = 0;
+                    song2.volume = 1;
+                    gameresult();
+                    initresult();
+                    clearInterval(gamesong);
+                    clearInterval(gamenote);
+                }
+            }
+        }
     }
     init();
 }
-
-//function roundedRect(x, y){
-//    var gradient = cnt.createRadialGradient((2*x+135)/2, (2*y+16)/2, 0, (2*x+135)/2, (2*y+16)/2, 70);
-//    gradient.addColorStop(0, "#ffff1a");
-//    gradient.addColorStop(1, "white");
-//    cnt.clearRect(0, 0, 1024, 576);
-//    cnt.beginPath();
-//    cnt.moveTo(x, y);
-//    cnt.lineTo(x+131, y);
-//    cnt.quadraticCurveTo(x+135, y, x+135, y+4);
-//    cnt.lineTo(x+135, y+12);
-//    cnt.quadraticCurveTo(x+135, y+16, x+131, y+16);
-//    cnt.lineTo(x+4, y+16);
-//    cnt.quadraticCurveTo(x, y+16, x, y+12);
-//    cnt.lineTo(x, y+4);
-//    cnt.quadraticCurveTo(x, y, x+4, y);
-//    cnt.lineWidth = 3;
-//    cnt.strokeStyle = "#333333";
-//    cnt.stroke()
-//    cnt.closePath();
-//    cnt.fillStyle = gradient;
-//    cnt.fill();
-//}
+function randomnum(){
+    noteloc = Math.floor(Math.random() * 4);
+    everything[i] = noteloc;
+    i++;
+}
 
 function init(){
     var gradient = cnt.createLinearGradient(0, 500, 0, 620);
@@ -142,15 +200,7 @@ function init(){
     know_key(gradient, "blue", 522, "D", 573);
     know_key(gradient, "green", 677, "F", 728);
     
-    cnt.beginPath();
-    cnt.globalAlpha = 0.8;
-    cnt.fillStyle = "#cccccc"
-    cnt.fillRect(0, 485, 1024, 10);
-    cnt.fill();
-    cnt.closePath();
-    cnt.globalAlpha = 1;
-    
-    game = requestAnimationFrame(game_start);
+    gameid = setInterval(draw, 5);
 }
 
 function know_key(gradient, gracolor, x, alpha, alphax){
@@ -164,4 +214,91 @@ function know_key(gradient, gracolor, x, alpha, alphax){
     cnt.font = "bold 50px Courier New";
     cnt.fillStyle = "white";
     cnt.fillText(alpha, alphax, 565);
+}
+
+function draw(){
+    noteY += noteV;
+    if(noteY >= 504){
+        noteY = 0;
+        judgetext.innerHTML = "MISS";
+        setTimeout(disappear, 300);
+        combotext.innerHTML = "";
+        combo = 0;
+        misssum += 1;
+        subi++;
+    }
+    cnt.clearRect(210, 0, 1024, 520);
+    roundedRect(212+(155*everything[subi]), noteY);
+}
+
+function roundedRect(x, y){
+    var gradient = cnt.createRadialGradient((2*x+135)/2, (2*y+16)/2, 0, (2*x+135)/2, (2*y+16)/2, 70);
+    gradient.addColorStop(0, "#ffff1a");
+    gradient.addColorStop(1, "white");
+    cnt.beginPath();
+    cnt.moveTo(x, y);
+    cnt.lineTo(x+131, y);
+    cnt.quadraticCurveTo(x+135, y, x+135, y+4);
+    cnt.lineTo(x+135, y+12);
+    cnt.quadraticCurveTo(x+135, y+16, x+131, y+16);
+    cnt.lineTo(x+4, y+16);
+    cnt.quadraticCurveTo(x, y+16, x, y+12);
+    cnt.lineTo(x, y+4);
+    cnt.quadraticCurveTo(x, y, x+4, y);
+    cnt.lineWidth = 3;
+    cnt.strokeStyle = "#333333";
+    cnt.stroke()
+    cnt.closePath();
+    cnt.fillStyle = gradient;
+    cnt.fill();
+}
+
+function gameresult(){
+    if(resultscreen.style.visibility == "hidden"){
+        gamescreen.style.visibility = "hidden";
+        resultscreen.style.visibility = "visible";
+    }
+    else
+        resultscreen.style.visibility = "hidden";
+    
+    
+    allresult[0].innerHTML = "Score: " + Math.round(sum);
+    allresult[2].innerHTML = "Combo: " + maxcombo;
+    allresult[3].innerHTML = "Perfect: " + perfectsum;
+    allresult[4].innerHTML = "Good: " + goodsum;
+    allresult[5].innerHTML = "Bad: " + badsum;
+    allresult[6].innerHTML = "Miss: " + misssum;
+    allresult[8].innerHTML = "Rank: " + rankdecision(Math.round(sum));
+}
+
+function initresult(){
+    combo = 0;
+    sum = 0;
+    perfectsum = 0;
+    goodsum = 0;
+    badsum = 0;
+    misssum = 0;
+    noteY = 0;
+    combotext.innerHTML = "";
+    clearInterval(gameid);
+    resultscreen.reset();
+}
+
+function gomain(){
+    if(choosescreen.style.visibility === "hidden"){
+        resultscreen.style.visibility = "hidden";
+        choosescreen.style.visibility = "visible";
+    }
+    else{
+        choosescreen.style.visibility = "hidden";
+    }
+    mus2.disabled = true;
+    song1.play();
+    prelisten = setInterval(songloop, 5);
+    function songloop(){
+        var time = song1.currentTime;
+        if(time > 40){
+            song1.currentTime = 0;
+        }
+    }
 }
