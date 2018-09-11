54e638fb4990da80764433ec999a50291e78b7aa
bloquer <995skh@gmail.com>
2018-09-11 20:51:23 +0900

    initial commit

--- Java/10Chapter_board/src/haker/BoardCommand.java
+++
@@ -1,8 +0,0 @@
-package haker;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BoardCommand {
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
--- Java/Practice/WebContent/ch7/response_sendRedirect.jsp
+++
@@ -1,2 +0,0 @@
-<!--학번 : 2014041030 이름: 손광호  -->
-<%response.sendRedirect("page_control_end.jsp"); %>
