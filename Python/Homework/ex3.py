206ccbcc2d9bf744447f1d9a3c6f5180d2e2b25e
bloquer <995skh@gmail.com>
2018-10-06 14:25:19 +0900

    initial commit

--- Python/Homework/ex3.py
+++ Python/Homework/ex3.py
@@ -1,15 +1,55 @@
 class Brackets:
+    change_result = list()
     count = 0
     def __init__(self, bra):
         self.bra = bra
+    def switch(self, x):
+        return {'(':1, '{':2, '[':3, ')':4, '}':5, ']':6}.get(x, 0)
+    def changenum(self):
+        for i in self.bra:
+            Brackets.change_result.append(Brackets.switch(i))
     def check(self):
+        for i in self.bra:
+            if(i == '('):
+                
+
+        if(len(self.bra) % 2 != 0):
+            return "Invalid"
+        for i in range(len(self.bra) + 1):
+            rev = (-1 * i) - 1
+            if(self.bra[i] == '('):
+                if(self.bra[rev] == ')' or self.bra[i + 1] == ')'):
+                    continue
+            elif (self.bra[i] == '('):
+                if (self.bra[rev] == ')' or self.bra[i + 1] == ')'):
+                    continue
+            elif (self.bra[i] == '('):
+                if (self.bra[rev] == ')' or self.bra[i + 1] == ')'):
+                    continue
+
+
+
+        for i in Brackets.change_result:
+            if(i > 3):
+                i -= 3
+        temp1 = Brackets.change_result
+        temp2 = Brackets.change_result.reverse()
+
+
+        if(len(temp1) % 2 == 1):
+            return "Invalid"
+        if(temp1 == temp2):
+            return  "Valid"
+        else:
+            for()
+        Brackets.count += 1
+        self.check()
         if(self.bra[Brackets.count] == '(' or self.bra[Brackets.count] == '{' or self.bra[Brackets.count] == '['):
             if(Brackets.count >= int(len(self.bra)/2)):
                 return "Invalid"
             Brackets.count += 1
             self.Check()
         elif(self.bra[Brackets.count] == ')' or self.bra[Brackets.count] == '}' or self.bra[Brackets.count] == ']'):
-
             if(Brackets.count < int(len(self.bra)/2)):
                 return "Invalid"
             Brackets.count += 1
--- Python/Homework/ex4.py
+++ Python/Homework/ex4.py
@@ -1,28 +1,56 @@
+# target 값을 만족하는 인덱스를 찾는 클래스 정의
 class FindPair:
+    # init 함수 정의: number 리스트를 받는다
     def __init__(self, numb):
         self.numb = numb
+
+    # target을 만족하는 두 쌍의 인덱스를 찾는 함수 정의
     def find(self, target):
+        # 리스트 원소 개수만큼 반복
         for i in self.numb:
+            # target 값을 임시로 저장
             target_num = target
+
+            # target 값보다 큰 값은 두 쌍을 구할 수 없기 때문에 패스
             if (i >= target_num):
                 continue
+
+            # 첫번째 값의 인덱스를 저장
             a = self.numb.index(i)
+            # 나머지 값을 찾기 위해 target값에서 첫번째 값을 뺀다
             target_num -= i
+
+            # 그 값이 리스트에 존재하지 않을 수도 있기 때문에 예외 처리를 해준다
             try:
+                # 찾은 두번째 값의 인덱스를 저장한다
                 b = self.numb.index(target_num)
+                # 인덱스가 같다는 것은 중복되었다는 뜻이므로 패스한다
                 if (a == b):
                     continue
+            # 리스트에 두번째 값이 존재하지 않으면 패스
             except:
                 continue
+
+            # 첫번째, 두번째 값의 인덱스를 출력
             print("Output: %d, %d" % (a, b))
 
+# 공백 리스트 생성
 numbers = list()
+
+# 사용자에게 배열을 입력받는다
 while True:
     num = int(input("배열 num 입력(0입력 시 종료): "))
+    # 0을 입력받으면 반복문을 탈출해 입력 종료
     if num == 0:
         break
+    # 입력받은 값을 리스트에 추가
     numbers.append(num)
+# 입력받은 배열을 보여준다
 print(numbers)
+# target number의 값을 입력받는다
 target = int(input("target 값 입력: "))
+
+# FindPair 클래스를 선언
 finding = FindPair(numbers)
+# target을 만족하는 두 쌍의 인덱스 값을 찾는 함수 실행
 finding.find(target)
