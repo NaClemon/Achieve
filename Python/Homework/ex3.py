3d1d81443d555700a2bd41694236be8c76dc2974
bloquer <995skh@gmail.com>
2018-10-06 21:56:26 +0900

    initial commit

--- Python/Homework/ex3.py
+++ Python/Homework/ex3.py
@@ -1,59 +1,48 @@
+# 괄호문의 유효성을 검사하는 클래스 정의
 class Brackets:
-    change_result = list()
-    count = 0
+    # 초기화 메소드로 문자열을 받아온다
     def __init__(self, bra):
-        self.bra = bra
-    def switch(self, x):
-        return {'(':1, '{':2, '[':3, ')':4, '}':5, ']':6}.get(x, 0)
-    def changenum(self):
-        for i in self.bra:
-            Brackets.change_result.append(Brackets.switch(i))
+        self.bra = bra\
+    # 문자열의 유효성을 검사하는 함수 정의
     def check(self):
+        # 문자열을 한개씩 읽으며 검사
         for i in self.bra:
-            if(i == '('):
-                
+            try:
+                # 배열의 다음 값 또는 뒤에서 부터의 값을 찾기 위해 인덱스+1을 저장
+                temp = self.bra.index(i) + 1
+                # 괄호가 '('일때
+                if(i == '('):
+                    # 다음 기호 또는 뒷부분에 닫는 기호가 있는지 검사
+                    if(self.bra[temp] == ')' or self.bra[(-1 * temp)] == ')'):
+                        # 있다면 계속
+                        continue
+                    # 없다면 유효하지 않음
+                    return "Invalid"
 
-        if(len(self.bra) % 2 != 0):
-            return "Invalid"
-        for i in range(len(self.bra) + 1):
-            rev = (-1 * i) - 1
-            if(self.bra[i] == '('):
-                if(self.bra[rev] == ')' or self.bra[i + 1] == ')'):
-                    continue
-            elif (self.bra[i] == '('):
-                if (self.bra[rev] == ')' or self.bra[i + 1] == ')'):
-                    continue
-            elif (self.bra[i] == '('):
-                if (self.bra[rev] == ')' or self.bra[i + 1] == ')'):
-                    continue
+                # '('의 과정과 동일
+                elif(i == '{'):
+                    if (self.bra[temp] == '}' or self.bra[(-1 * temp)] == '}'):
+                        continue
+                    return "Invalid"
 
+                # '('의 과정과 동일
+                elif(i == '['):
+                    if (self.bra[temp] == ']' or self.bra[(-1 * temp)] == ']'):
+                        continue
+                    return "Invalid"
 
-
-        for i in Brackets.change_result:
-            if(i > 3):
-                i -= 3
-        temp1 = Brackets.change_result
-        temp2 = Brackets.change_result.reverse()
-
-
-        if(len(temp1) % 2 == 1):
-            return "Invalid"
-        if(temp1 == temp2):
-            return  "Valid"
-        else:
-            for()
-        Brackets.count += 1
-        self.check()
-        if(self.bra[Brackets.count] == '(' or self.bra[Brackets.count] == '{' or self.bra[Brackets.count] == '['):
-            if(Brackets.count >= int(len(self.bra)/2)):
+            # 예외 처리(배열의 원소가 하나일 때)
+            except IndexError:
+                # '(', '}' 등 한개이면 또는 마지막에 있으면 + 1 시 오류
+                # 이 경우는 유효하지 않으므로 예외 처리로 Invalid 출력
                 return "Invalid"
-            Brackets.count += 1
-            self.Check()
-        elif(self.bra[Brackets.count] == ')' or self.bra[Brackets.count] == '}' or self.bra[Brackets.count] == ']'):
-            if(Brackets.count < int(len(self.bra)/2)):
-                return "Invalid"
-            Brackets.count += 1
-            self.Check()
 
-strcheck = Brackets(input("입력하세요."))
-print(strcheck.Check())
+        # 이상 없이 for문을 다 돌면 Valid 출력
+        return "Valid"
+
+# 사용자로부터 문자열을 입력받는다
+# 그 문자열을 매개변수로 괄호 검사 클래스 선언
+strcheck = Brackets(input("입력하세요: "))
+
+# 문자열의 유효성 검사후 결과 출력
+print(strcheck.check())
