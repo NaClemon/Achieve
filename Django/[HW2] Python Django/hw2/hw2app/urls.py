191ab494d0394df7967bde76f476cea8fa11843f
bloquer <995skh@gmail.com>
2018-11-15 00:07:26 +0900

    initial commit

--- Django/[HW2] Python Django/hw2/hw2app/urls.py
+++ Django/[HW2] Python Django/hw2/hw2app/urls.py
@@ -17,5 +17,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('home', views.home, name='home'),
+    path('note', views.note, name='note'),
+    path('list', views.list, name='list')
 ]
+
--- Django/[HW2] Python Django/hw2/hw2app/views.py
+++ Django/[HW2] Python Django/hw2/hw2app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.contrib import messages
+import json
 
 # Create your views here.
 def home(request):
     return render(request, 'content/main.html')
+
+notes = []
+title = ""
+note = ""
+
+def note(request):
+    return render(request, 'content/note.html')
+
+def list(request):
+    if request.method == "POST":
+        try:
+            title = request.POST.get("title")
+            note = request.POST.get("note")
+            notes.append([title, note])
+        except Exception as e:
+            messages.error(request, repr(e))
+    return render(request, 'content/list.html', {'List': title, 'Note': note})
