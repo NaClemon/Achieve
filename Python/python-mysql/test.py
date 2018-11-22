c0b81411140dff62c69db143c2f8b6e978db49bf
bloquer <995skh@gmail.com>
2018-11-22 13:53:25 +0900

    initial commit

--- Python/python-mysql/test.py
+++ Python/python-mysql/test.py
@@ -1,11 +1,20 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    passwd='tmvjswl@2877'
+    host='cbnuclouddb.cn25twnqpp2k.ap-northeast-2.rds.amazonaws.com',
+    user='cbnuclouddb',
+    passwd='aziz1234',
+    database='skh'
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE mydatabase")
+sql2 = 'create table skh2 (num int not null primary key, name varchar(32), age int)'
+sql1 = 'create table skh1 (id int not null primary key, name varchar(32) not null' \
+            ', email varchar(32) not null, guide_num int, foreign key(guide_num) references' \
+       'skh2(num))'
+
+sql_test = 'show table'
+
+mycursor.execute(sql_test)
+
